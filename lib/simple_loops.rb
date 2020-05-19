376bd10dcff5494f0fd7562c9637f6c9415e3d5e
Julio Chazari <julio.chazari@gmail.com>
2020-05-19 04:42:44 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,12 +2,16 @@ def loop_message_five_times(string)
   counter = 0
   while counter <= 5 do
     puts string
+    counter += 1
   end
 end
 
 def loop_message_n_times(string, integer)
   counter = 0
-  while 
+  while counter <= integer do
+    puts string
+    counter += 1
+  end
 end
 
 def output_array

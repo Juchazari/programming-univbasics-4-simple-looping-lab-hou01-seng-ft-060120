d1b285c256d099313bdf6639304d6c86f003c7f2
Julio Chazari <julio.chazari@gmail.com>
2020-05-19 04:44:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,8 +14,11 @@ def loop_message_n_times(string, integer)
   end
 end
 
-def output_array
-  
+def output_array(array)
+  counter = 0
+  while counter < array.length do
+    puts array[counter]
+  end
 end
 
 def return_string_array
